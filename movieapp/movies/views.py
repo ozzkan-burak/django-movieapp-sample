6745d33263ce29aee5a0b6744964e426db6fcf29
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-05-07 14:58:35 +0300

    episode2

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the movies index page.")
+
+def movies(request):
+    return HttpResponse("Hello, world. You're at the movies page.")
+
+def movie_details(request, slug):
+    return HttpResponse("Hello, world. You're at the " + slug + " movie details page.")
